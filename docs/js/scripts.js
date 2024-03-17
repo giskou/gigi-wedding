aae8e3f349de3f2090a6f7b240ff48a7f2bc242a
Giannis Skoulis <Ioannis.Skoulis@forsta.com>
2024-03-17 22:49:56 +0100

    Add story and various css fixes

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -5,3 +5,62 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// countdown
+function getTimeRemaining(endtime){
+  const total = Date.parse(endtime) - Date.parse(new Date());
+  const seconds = Math.floor( (total/1000) % 60 );
+  const minutes = Math.floor( (total/1000/60) % 60 );
+  const hours = Math.floor( (total/(1000*60*60)) % 24 );
+  const days = Math.floor( total/(1000*60*60*24) );
+
+  return {
+    total,
+    days,
+    hours,
+    minutes,
+    seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  const clock = document.getElementById(id);
+  const timeinterval = setInterval(() => {
+    const t = getTimeRemaining(endtime);
+    clock.innerHTML = t.days + 'd' +
+                      ' '+ t.hours + 'h' +
+                      ' ' + t.minutes + 'm' +
+                      ' ' + t.seconds + 's';
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  },1000);
+}
+
+const deadline = 'July 13 2024 18:30:00 GMT+0200';
+initializeClock('countdown', deadline);
